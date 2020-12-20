ce0265e1c54a1f084cf97f22d97f811da3c63dd5
Ravi Sri Ram Chowdary <ravisriramchowdary@gmail.com>
2020-12-20 19:41:49 +0530

    Moved theme functionality to the redux store.

--- src/App.js
+++ src/App.js
@@ -15,70 +15,27 @@ import About from "./views/About";
 import Movie from "./views/Movie";
 import Error404 from "./main/Error404";
 import PrivateRoute from "./main/PrivateRoute.js";
+import { connect } from 'react-redux';
 
-export default class App extends React.Component {
-  render() {
-    let name, color, backimg, value, hr;
-    hr = new Date().getHours();
-    if (hr >= 1 && hr < 4) value = "Midnight";
-    else if (hr >= 4 && hr < 6) value = "Dawn";
-    else if (hr >= 6 && hr < 9) value = "Morning";
-    else if (hr >= 9 && hr < 12) value = "Midmorning";
-    else if (hr >= 12 && hr < 17) value = "Afternoon";
-    else if (hr >= 17 && hr < 20) value = "Dusk";
-    else if (hr >= 20 && hr < 22) value = "Evening";
-    else value = "Night";
-    switch (value) {
-      case "Midnight":
-        name = "dark";
-        color = "#afbda6";
-        backimg = "/assets/firewatch_1.jpg";
-        break;
-      case "Dawn":
-        name = "dark";
-        color = "#f2c548";
-        backimg = "/assets/firewatch_2.jpg";
-        break;
-      case "Morning":
-        name = "light";
-        color = "#306458";
-        backimg = "/assets/firewatch_3.jpg";
-        break;
-      case "Midmorning":
-        name = "light";
-        color = "#3c6e8f";
-        backimg = "/assets/firewatch_4.jpg";
-        break;
-      case "Afternoon":
-        name = "light";
-        color = "#9a5126";
-        backimg = "/assets/firewatch_5.jpg";
-        break;
-      case "Dusk":
-        name = "dark";
-        color = "#f5ba54";
-        backimg = "/assets/firewatch_6.jpg";
-        break;
-      case "Evening":
-        name = "dark";
-        color = "#95d1e9";
-        backimg = "/assets/firewatch_7.jpg";
-        break;
-      default:
-        name = "dark";
-        color = "#a0a5ab";
-        backimg = "/assets/firewatch_8.jpg";
+class App extends React.Component {
+
+  shouldComponentUpdate(nextProps, nextState){
+    if(nextProps.theme.color !== this.props.theme.color || nextProps.theme.color === this.props.theme.color ){
+      return false;
     }
+  }
 
+  render() {
+    
     return (
       <BrowserRouter>
         <div className="App">
           <Theme
             theme={getTheme({
-              themeName: name,
-              accent: color,
+              themeName: this.props.theme.name,
+              accent: this.props.theme.color,
               useFluentDesign: true,
-              desktopBackgroundImage: backimg
+              desktopBackgroundImage: this.props.theme.backimg
             })}
           >
             <Switch>
@@ -102,3 +59,12 @@ export default class App extends React.Component {
     );
   }
 }
+const mapStateToProps = ( state ) => {
+  return {
+    theme : state.Theme
+  }
+}
+
+const mapDispatchToProps = null
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import {createStore} from 'redux';
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store = { store }>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
